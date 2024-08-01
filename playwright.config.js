1f135a4126e0a97dd4e1b5ebc3c8bd9c0d7ea0ec
Danny Tran <beepboop@Dannys-iMac-Pro.local>
2024-08-01 13:09:50 +1000

    ci: play around with github action reporters for playwright

--- playwright.config.js
+++ playwright.config.js
@@ -1,25 +1,40 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: 'src/tests/e2e', // Make sure this matches your directory structure
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
-  use: {
-    baseURL: 'http://127.0.0.1:3000',
-    trace: 'on-first-retry'
-  },
+  testDir: 'src/tests/e2e',
+  reporter: process.env.CI ? 'github' : 'list',
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] }
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] }
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] }
+    },
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] }
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] }
+    },
+    {
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' }
     }
   ],
+  use: {
+    baseURL: 'http://127.0.0.1:3000',
+    trace: 'on-first-retry'
+  },
   webServer: {
     command: 'npm run start',
-    url: 'http://127.0.0.1:3000',
-    reuseExistingServer: !process.env.CI
+    url: 'http://127.0.0.1:3000'
   }
 });
