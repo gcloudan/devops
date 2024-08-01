cd4f6e3f23fd053682baa8f29f67e206a05bf1f4
Danny Tran <nabdtran@gmail.com>
2024-08-01 13:44:40 +1000

    ci: try services

---
+++ src/tests/e2e/api.todo.js
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test';
+
+test('Backend Test - Auth EP is up', {
+  tag: ['@ProjectName', '@Tests', '@API', '@BackendTest']
+}, async ({ request }) => {
+  const response = await request.get(`/new`);
+
+  // Assertion
+  expect(response.status()).toBe(200);
+});
+
+test('Backend Test - Create data', {
+  tag: ['@ProjectName', '@Tests', '@AuthUser', '@Login', '@API', '@BackendTest'] }, async ({ request }) => {
+  const response = await request.post("/", {
+    title: "hi",
+    description: "test"
+  });
+
+  // Assertion
+  expect(response.status()).toBe(200);
+});
+
+test('Backend Test - Delete data', {
+  tag: ['@ProjectName', '@Tests', '@API', '@BackendTest'] }, async ({ request }) => {
+  const response = await request.post("/", {
+    title: "hi",
+    description: "test"
+  });
+
+  // Assertion
+  expect(response.status()).toBe(200);
+});
+
+
+    // await Note.findByIdAndRemove(req.params.id);
+
